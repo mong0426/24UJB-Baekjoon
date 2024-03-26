7ea86cccf0279031b23af5e5dd07208d31fa8d72
21-manic-020g <21-manic-020g@21-manic-020>
2024-03-26 15:45:30 +0900

    Initial commit

---
+++ Backjun/src/q2741/Main.java
@@ -0,0 +1,18 @@
+package q2741;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		sc.close();
+		
+		for(int i=1;i<num+1;i++) {
+			System.out.println(i);
+		}
+		
+	}
+
+}
